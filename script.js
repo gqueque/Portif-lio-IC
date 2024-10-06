615b4a268cd142877da766292c456430b6f477c5
Gustavo Queque <61836852+gqueque@users.noreply.github.com>
2024-10-06 18:36:09 -0300

    1° Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // Torna a paginação clicável
+  },
+  mousewheel: true,
+  keyboard: true,
+});
